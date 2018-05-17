47bec350355fe007724ee3111a165cd7aece7dc2
n0dam1 <minoru.noda.0527@gmail.com>
2018-05-17 19:52:24 +0900

    トップページのスケルトン作成

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module RailsSampleApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.generators do |t|
+    config.generators do |g|
       g.skip_routes true
       g.stylesheets false
       g.javascripts false
